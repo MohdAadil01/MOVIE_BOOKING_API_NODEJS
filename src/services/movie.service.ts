ab56dbdaeb9e6ba4005ae1161c6957478a006021
MohdAadil01 <mohdaadil4240@gmail.com>
2025-12-20 22:31:28 +0530

    Feat: use redis at service layer

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,5 +1,6 @@
 import { Movie } from "../models/movie.model";
 import { AppError } from "../utils/appError";
+import { deleteCache, getCache, setCache } from "../utils/cache";
 import {
   CreateMovieInput,
   UpdateMovieInput,
@@ -13,7 +14,7 @@ export const createMovieService = async (data: CreateMovieInput) => {
   }
 
   const movie = await Movie.create(data);
-
+  await deleteCache(`movie:all`);
   return movie;
 };
 
@@ -21,19 +22,35 @@ export const findMovieByIdService = async (id: string) => {
   if (!id) {
     throw new AppError("Id not provided", 400);
   }
+  const cacheKey = `movie:id:${id}`;
+  const cachedMovie = await getCache(cacheKey);
+  if (cachedMovie) {
+    return cachedMovie;
+  }
+
   const movie = await Movie.findById(id);
 
   if (!movie) {
     throw new AppError("Movie is not available", 404);
   }
+
+  await setCache(cacheKey, movie, 900);
   return movie;
 };
 
 export const findAllMoviesService = async () => {
+  const cacheKey = `movie:all`;
+  const cachedMovies = await getCache(cacheKey);
+  if (cachedMovies) {
+    return cachedMovies;
+  }
+
   const allMovies = await Movie.find({});
   if (allMovies.length === 0) {
     throw new AppError("Nothing available here", 404);
   }
+
+  await setCache(cacheKey, allMovies, 900);
   return allMovies;
 };
 
@@ -46,7 +63,8 @@ export const deleteMovieService = async (id: string) => {
     throw new AppError("Movie not found", 404);
   }
   await movie.deleteOne();
-
+  await deleteCache(`movie:id:${id}`);
+  await deleteCache(`movie:all`);
   return movie;
 };
 
@@ -61,5 +79,9 @@ export const updateMovieService = async (
   if (!movie) {
     throw new AppError("Movie not found", 404);
   }
+
+  await deleteCache(`movie:id:${id}`);
+  await deleteCache(`movie:all`);
+
   return movie;
 };
--- src/services/theatre.service.ts
+++ src/services/theatre.service.ts
@@ -1,5 +1,6 @@
 import { Theatre } from "../models/theatre.model";
 import { AppError } from "../utils/appError";
+import { deleteCache, getCache, setCache } from "../utils/cache";
 import {
   CreateTheatreInput,
   UpdateTheatreInput,
@@ -16,6 +17,7 @@ export const createTheatreService = async (data: CreateTheatreInput) => {
   }
 
   const theatre = await Theatre.create(data);
+  await deleteCache(`theatres:all`);
   return theatre;
 };
 
@@ -32,6 +34,8 @@ export const updateTheatreService = async (
   }
 
   const theatre = await Theatre.findByIdAndUpdate(id, data);
+  await deleteCache(`theatres:id:${id}`);
+  await deleteCache(`theatres:all`);
   return theatre;
 };
 
@@ -45,6 +49,10 @@ export const deleteTheatreService = async (id: string) => {
   }
 
   const theatre = await Theatre.findByIdAndDelete(id);
+
+  await deleteCache(`theatres:id:${id}`);
+  await deleteCache(`theatres:all`);
+
   return theatre;
 };
 
@@ -52,17 +60,34 @@ export const getSingleTheatreService = async (id: string) => {
   if (!id) {
     throw new AppError("Theatre id not provided", 400);
   }
+
+  const cacheKey = `theatres:id:${id}`;
+  const cachedTheatre = await getCache(cacheKey);
+  if (cachedTheatre) {
+    return cachedTheatre;
+  }
+
   const existingTheatre = await Theatre.findById(id);
   if (!existingTheatre) {
     throw new AppError("No theatre found", 404);
   }
+
+  await setCache(cacheKey, existingTheatre, 900);
   return existingTheatre;
 };
 
 export const getAllTheatreService = async () => {
+  const cacheKey = `theatres:all`;
+  const cachedTheatres = await getCache(cacheKey);
+  if (cacheKey) {
+    return cachedTheatres;
+  }
+
   const theatres = await Theatre.find({});
   if (theatres.length === 0) {
     throw new AppError("No theatre found", 404);
   }
+
+  await setCache(cacheKey, theatres, 900);
   return theatres;
 };
